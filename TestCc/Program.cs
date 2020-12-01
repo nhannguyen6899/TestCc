9439838f8b758adec349653d19907189fb290299
= <=>
2020-12-01 19:36:56 +0700

    Add project files.

---
+++ TestCc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestCc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
